15977b7fc271403a090a9ed6aa6723531a7680b7
Santosh Patil <santosh.a.patil@gmail.com>
2019-12-20 23:54:17 +1100

    Add Grading Students

---
+++ Grading_students/solution.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    temp = []
+    idx = 0
+    for i in grades:
+        if i>=38 and (i%5)>2:
+            i=i+(5-(i%5))
+        temp.insert(idx,i)
+        idx=idx + 1
+    return temp
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
+
